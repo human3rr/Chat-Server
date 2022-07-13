5b1f2deeee501f14f216fa4decd102d3035a814c
Noah Brewer <noah.t.brewer@gmail.com>
2022-07-12 20:36:08 -0500

    wip

--- index.js
+++ index.js
@@ -5,15 +5,22 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+usersConnected = 0
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
   });
 
   io.on('connection', (socket) => {
     console.log('a user connected');
+    io.emit('usersConnected', ++usersConnected)
+    console.log("users: " + usersConnected)
     io.emit('chat message', socket.id + " joined");
     socket.on('disconnect', (usr) => {
       console.log('user disconnected');
+      io.emit('usersConnected', --usersConnected)
+      console.log("users: " + usersConnected)
+    io.emit('chat message', socket.id + " joined");
       io.emit('chat message', socket.id + " disconnected");
     });
     socket.on('chat message', (msg) => {
